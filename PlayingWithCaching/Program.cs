b3400f87688744893534cf20c30e491bc4234f0e
Balazs Kereszturi <19balazs86@gmail.com>
2024-09-27 18:00:05 +0200

    Add some example of cashing

--- PlayingWithCaching/Program.cs
+++ PlayingWithCaching/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.OutputCaching;
+
 namespace PlayingWithCaching;
 
 public static class Program
@@ -10,6 +12,18 @@ public static class Program
         // Add services to the container
         {
             services.AddAuthorization();
+
+            services.AddOutputCache(options =>
+            {
+                options.AddBasePolicy(builder =>
+                {
+                    builder.NoCache()
+                           .Expire(TimeSpan.FromSeconds(20))
+                           .Tag("tag-all");
+                });
+
+                options.AddPolicy("Expire-1-min", builder => builder.Expire(TimeSpan.FromSeconds(60)));
+            });
         }
 
         WebApplication app = builder.Build();
@@ -18,11 +32,24 @@ public static class Program
         {
             app.UseAuthorization();
 
-            app.MapGet("/", Gravatar.WriteGravatar);
+            app.UseOutputCache();
+
+            app.MapGet("/", Gravatar.WriteGravatar); // NoCache by default
+
+            app.MapGet("/default", Gravatar.WriteGravatar).CacheOutput(); // It uses the BasePolicy
+
+            app.MapGet("/expire1min", Gravatar.WriteGravatar).CacheOutput("Expire-1-min");
+
+            app.MapGet("/evict/{tag}", handleEvictByTag);
         }
 
         app.UseAuthorization();
 
         app.Run();
+    }
+
+    private static async Task handleEvictByTag(string tag, IOutputCacheStore cache)
+    {
+        await cache.EvictByTagAsync(tag, default);
     }
 }
