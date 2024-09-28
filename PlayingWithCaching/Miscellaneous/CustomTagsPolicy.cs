683407a1bbf3137376f17f60c36f2261cc428ead
Balazs Kereszturi <19balazs86@gmail.com>
2024-09-28 09:37:34 +0200

    Add CustomTagsPolicy

---
+++ PlayingWithCaching/Miscellaneous/CustomTagsPolicy.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.OutputCaching;
+
+namespace PlayingWithCaching.Miscellaneous;
+
+public sealed class CustomTagsPolicy : IOutputCachePolicy
+{
+    private readonly Func<HttpContext, string[]> _tagsFunc;
+
+    public CustomTagsPolicy(Func<HttpContext, string[]> tagsFunc)
+    {
+        _tagsFunc = tagsFunc;
+    }
+
+    public ValueTask CacheRequestAsync(OutputCacheContext context, CancellationToken cancellation)
+    {
+        foreach (string tag in _tagsFunc(context.HttpContext))
+        {
+            context.Tags.Add(tag);
+        }
+
+        return ValueTask.CompletedTask;
+    }
+
+    public ValueTask ServeFromCacheAsync(OutputCacheContext context, CancellationToken cancellation)
+    {
+        return ValueTask.CompletedTask;
+    }
+
+    public ValueTask ServeResponseAsync(OutputCacheContext context, CancellationToken cancellation)
+    {
+        return ValueTask.CompletedTask;
+    }
+}
